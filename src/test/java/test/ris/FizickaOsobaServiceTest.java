6a714ca168a1107a4e8d3fe7087bef9feb05e6b0
dariansaric <darian.saric@fer.hr>
2019-06-06 18:51:59 +0200

    Svi potrebni testovi napravljeni

--- src/test/java/test/ris/FizickaOsobaServiceTest.java
+++ src/test/java/test/ris/FizickaOsobaServiceTest.java
@@ -1,5 +1,6 @@
 package test.ris;
 
+import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,15 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import test.ris.business.FizickaOsoba;
 import test.ris.business.FizickaOsobaService;
-import static org.junit.Assert.*;
 
 import java.sql.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class FizickaOsobaServiceTest {
     //sve prolazi ako se zasebno pokrenu
+    private static final Gson GSON = new Gson();
 
     @Autowired
     private MockMvc mvc;
@@ -33,6 +40,7 @@ public class FizickaOsobaServiceTest {
     @Test
     public void testIfMatchingPruzateljExists() {
         FizickaOsoba o = new FizickaOsoba("00003", "Luka", "Lukić", Date.valueOf("1900-1-1"));
+        //todo:drukčije testirati
         assertNull(service.saveNewFizickaOsoba(o));
         o.setOib("00002");
         assertEquals(o, service.saveNewFizickaOsoba(o));
@@ -42,7 +50,24 @@ public class FizickaOsobaServiceTest {
     public void testFailedSaveExisting() {
         FizickaOsoba o = new FizickaOsoba("00001", "Luka", "Lukić", Date.valueOf("1900-1-1"));
         assertNull(service.saveNewFizickaOsoba(o));
+        //todo:drukčije testirati
         o.setOib("00002");
         assertEquals(o, service.saveNewFizickaOsoba(o));
+    }
+
+    @Test
+    public void testSuccessfulSave() {
+        FizickaOsoba o = new FizickaOsoba("00002", "Luka", "Lukić", Date.valueOf("1900-1-1"));
+        assertEquals(o, service.saveNewFizickaOsoba(o));
+    }
+
+    @Test
+    public void testValidSaveAndRead() throws Exception {
+        FizickaOsoba o = new FizickaOsoba("00002", "Luka", "Lukić", Date.valueOf("1900-1-1"));
+//        FizickaOsoba o1 = service.saveNewFizickaOsoba(o);
+        testSuccessfulSave();
+        mvc.perform(get("/fosoba/00002"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(GSON.toJson(o)));
     }
 }
